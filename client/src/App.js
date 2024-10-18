677bb6d061749fbd12c2389d320f53546f02a956
Maryann-01 <amaraizummesoma@gmail.com>
2024-10-18 06:10:15 +0100

    feat:created a login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import Login from "./components/LoginPage/Login";
+import Register from "./components/RegisterPage/Register";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+
     </Routes>
   );
 };
