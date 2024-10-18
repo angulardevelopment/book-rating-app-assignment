38b96d2733864792a0470c6e3de14f2d6e43683e
Cheng Wang <chengwang1009@gmail.com>
2024-10-18 11:52:26 +0800

    book rating page consists of create and update function.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import BookRatingPage from "./components/BookRatingPage/BookRatingPage";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/book-rating" element={<BookRatingPage />}></Route>
     </Routes>
   );
 };
