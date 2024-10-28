bc733f49dcf5e8159d3787fe86c1e8db9001ff16
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-10-28 16:15:17 +0530

    Added the sample books

--- client/src/components/HomePage/Background.js
+++
@@ -1,7 +0,0 @@
-import styles from "./Background.module.css";
-
-function Background() {
-  return <div className={styles["hero-image"]}></div>;
-}
-
-export default Background;
--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -2,10 +2,47 @@ import React from "react";
 import "./home.css";
 import BooksList from "./BooksList";
 import { useBooks } from "../../contexts/BooksContext";
-import Background from "./Background";
+import Books from "../Books/Books";
+
 function HomePage() {
   const { query } = useBooks();
-  return <div>{query?.length > 3 ? <BooksList /> : <Background />}</div>;
+  
+  const sampleBooks = [
+    {
+      image: "https://spoo.me/tnetGb",
+      title: "Book 1",
+      author: "Author 1",
+      rating: 4.5
+    },
+    {
+      image: "https://spoo.me/tnetGb",
+      title: "Book 2",
+      author: "Author 2",
+      rating: 4.2
+    },
+    {
+      image: "https://spoo.me/tnetGb",
+      title: "Book 3",
+      author: "Author 3",
+      rating: 4.0
+    }
+  ];
+
+  return <div>{query?.length > 3 ? <BooksList /> : 
+  (
+    <div id="sampleBooks">
+          {sampleBooks.map((book, index) => (
+            <Books
+              key={index}
+              image={book.image}
+              title={book.title}
+              author={book.author}
+              rating={book.rating}
+            />
+          ))}
+        </div>
+  )
+  }</div>;
 }
 
 export default HomePage;
